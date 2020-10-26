a3bc096b9a2fed291fc2f37e85345e79ee95d67c
Briggette <briggette_1110@hotmail.com>
2020-10-26 15:51:33 -0500

    menu pag

---
+++ app_api/models/db.js
@@ -0,0 +1,20 @@
+
+
+
+const config = {
+    user: 'HR1011',
+    password: 'Bmet$1011',
+    server: 'localhost',
+    database: 'VerificadorPrecios',
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    },
+    parseJSON: true
+}
+
+module.exports = config;
+
+
+
