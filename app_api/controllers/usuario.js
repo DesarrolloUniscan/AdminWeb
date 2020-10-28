91218c661c5185158362e8ae2907724888344c29
NicolasHector <hrnicolas99@gmail.com>
2020-10-28 15:50:38 -0500

    Validar contraseña

--- app_api/controllers/usuario.js
+++ app_api/controllers/usuario.js
@@ -1,4 +1,4 @@
- 
+
 let sql = require('mssql');
 //const config = require('../models/db')
 
@@ -14,49 +14,64 @@ const config = {
         idleTimeoutMillis: 30000
     },
     parseJSON: true,
-    dialect:",ssql",
-    dialectOptiond:"SQLEXPRESS"
+    dialect: ",ssql",
+    dialectOptiond: "SQLEXPRESS"
 }
 
 
 
-const userCreate = (req, res) =>{
-    sql.connect(config).then(()=>{
+const userCreate = (req, res) => {
+    sql.connect(config).then(() => {
         return sql.query(`INSERT INTO ADMIN VALUES('${req.body.AD_usuario}', '${req.body.AD_password}')`);
     }).then(result => {
         res
             .status(200)
             .json(result)
-    }).catch(err =>{
+    }).catch(err => {
         res
             .status(400)
             .json(err)
     })
 }
 
-const userList = (req, res) =>{
-    sql.connect(config).then(()=>{
+const userList = (req, res) => {
+    sql.connect(config).then(() => {
         return sql.query("SELECT * FROM ADMIN");
-    }).then(result=>{
+    }).then(result => {
         res
             .status(200).send(result.recordset);
-            
-    }).catch(err=>{
+
+    }).catch(err => {
         res
             .status(404).send(err)
     })
 }
 
-const userRead =(req, res)=> {
-    sql.connect(config).then(()=>{
+const userRead = (req, res) => {
+    sql.connect(config).then(() => {
         console.log(req.params.usuarioid)
         return sql.query(`SELECT AD_usuario, AD_password FROM ADMIN WHERE AD_usuario='${req.params.usuarioid}'`);
-    }).then(result=>{
-        res
-            .status(200)
-            .json(result.recordset);
-            
-    }).catch(err=>{
+    }).then(result => {
+        if (result.recordset != "") {
+            console.log(result.recordset[0].AD_password)
+            if (result.recordset[0].AD_password == req.params.password) {
+                res
+                    .status(200)
+                    .json(result.recordset);
+            }else{
+                res
+                    .status(400)
+                    .json("Contraseña Incorrecta")
+            }
+
+        } else {
+            res
+                .status(404)
+                .json("Usuario no encontrado")
+        }
+
+
+    }).catch(err => {
         res
             .status(400)
             .json(err)
